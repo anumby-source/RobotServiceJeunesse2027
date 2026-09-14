e1e06185a424b817f2ab5a389487a0efda07a9a8
gildeb <48280441+gildeb@users.noreply.github.com>
2026-09-14 12:48:57 +0200

    Add files via upload

---
+++ Record_images/record_images_robot.py
@@ -0,0 +1,70 @@
+###################################################################
+#
+#   RSJ2025 : Robot code
+#
+###################################################################
+
+import network
+import espnow
+from time import sleep_ms, ticks_ms
+from machine import Pin, UART
+from dcMotor import dcMotor
+from mac_addr import telecommande_mac
+
+#
+num = 4
+telecommandeAddr = telecommande_mac[num]
+
+# motors initialization
+ml = dcMotor(pin1=5, pin2=6, pinEn=7, freq=10000)      # left motor
+mr = dcMotor(pin1=0, pin2=1, pinEn=2, freq=10000)      # right motor
+ml.duty_offset = 55
+mr.duty_offset = 55
+print("robot : motor init ok")
+
+# led initialization
+led = Pin(8, Pin.OUT)
+led.off()     # led on
+sleep_ms(1000)
+led.on()      # led off
+print("robot : led init ok")
+
+# UART initialization
+u = UART(0, rx=Pin(20, Pin.IN), tx=Pin(21, Pin.OUT))
+print("robot : UART init ok")
+
+# espnow initialization
+sta = network.WLAN(network.STA_IF) # A WLAN interface must be active
+sta.active(True)
+sta.disconnect()   # Because ESP8266 auto-connects to last Access Point
+print("robot : WLAN init ok")
+e = espnow.ESPNow()
+e.active(True)
+print("robot : espnow init ok")
+
+# add telecommande and base to peers
+try:
+    e.add_peer(telecommandeAddr)
+except:
+    pass         # if telecommande already in peer list
+print(b"robot : telecommande address added")
+#
+
+u.read(u.any())   # empty uart buffer
+msg = b''
+#
+while True:
+    if u.any():
+        msg += u.read(u.any())
+        if msg[-1] == 0x0a:  # last char = '\n'
+            e.send(telecommandeAddr, msg)
+            msg = b''
+    try:
+        addr, cmd = e.recv(0)
+#         if cmd: print(cmd)
+        if addr == telecommandeAddr:
+            exec(cmd)
+    except:
+        print(b"robot : error command:" + cmd)
+        led.off()    # led on
+        break
---
+++ Record_images/record_images_telecommande.py
@@ -0,0 +1,95 @@
+import network
+import espnow
+from machine import Pin, ADC
+from time import sleep_ms, ticks_ms
+from mac_addr import robot_mac, telecommande_mac
+import os
+import st7789
+import tft_config
+import vga2_bold_16x32 as font
+#
+num = 4
+robotAddr = robot_mac[num]
+
+#
+# red, green, blue, white, black = (0, 128, 0), (128, 0, 0), (0, 0, 128), (32, 32, 32), (0, 0, 0)
+
+#
+def normalize(x, amp=100):
+    ''' resize x -> [-amp;+amp] '''
+    x -= midadc
+    if x > dzw:
+        x = int(amp*(x-dzw)/szw)
+    elif x < -dzw:
+        x = int(amp*(x+dzw)/szw)
+    else:
+        x = 0
+    return x
+#
+def constrain(x):
+    return min(max(x, -100), 100)
+# init display
+tft = tft_config.config(3, buffer_size=4096)
+tft.init()
+# display logo
+png_file_name = '/russhughes/demos/Anumby-240x135.png'
+tft.png(png_file_name, 0, 0)
+sleep_ms(1000)
+# init ADC
+a0 = ADC(Pin(39, Pin.IN), atten=ADC.ATTN_11DB)
+a1 = ADC(Pin(36, Pin.IN), atten=ADC.ATTN_11DB)
+midadc = 1730         # adc middle
+maxadc = 3150         # adc max
+dzw    = 200          # dead zone width
+szw    = midadc - dzw # sensitive zone width
+samp   = 100
+ramp   = 50
+
+# init push buttons
+p0, s0   = Pin(0, Pin.IN), True
+p35, s35 = Pin(35, Pin.IN), True
+
+# A WLAN interface must be active to send()/recv()
+sta = network.WLAN(network.STA_IF)  # Or network.AP_IF
+sta.active(True)
+sta.disconnect()      # For ESP8266
+
+# init espnow
+e = espnow.ESPNow()
+e.active(True)
+print("telecommande.py : Network active, Espnow ok")
+
+try:
+    e.add_peer(robotAddr)      # Must add_peer() before send()
+except:
+    pass
+print("telecommande.py : robot added to peers")
+#
+while True:
+    try:
+        r, s = a0.read_uv()/1000, a1.read_uv()/1000
+        r = normalize(r, ramp)     # r in [-ramp,+ramp]
+        s = normalize(s, samp)     # s in [-samp,+samp]
+        ls, rs = constrain(s+r), constrain(s-r)
+        cmd = 'ml.set_speed(' + str(ls) + ')\r'
+        cmd += 'mr.set_speed(' + str(rs) + ')\r'
+        e.send(robotAddr, cmd.encode(), False)
+#         print(cmd.encode())
+        if s0 and (not p0.value()):
+            s0 = False
+            e.send(robotAddr, 'u.write("record")'.encode(), False)
+            print('u.write("record")'.encode())
+        elif (not s0) and p0.value():
+            s0 = True
+        addr, ans = e.recv(0)
+        if ans and (addr == robotAddr):
+            tft.fill(0)
+            tft.text(font, ans, 0, 0)
+            print(ans)
+        sleep_ms(100)
+    except:
+#         np[0] = red
+#         np.write()
+        break
+tft.fill(0)
+del tft
